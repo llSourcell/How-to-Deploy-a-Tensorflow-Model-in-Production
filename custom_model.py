845953697b11a10c40a8d70dd49df46be9ce4fa9
Siraj <sirajravel@gmail.com>
2017-05-30 13:44:47 -0700

    Update custom_model.py

--- custom_model.py
+++ custom_model.py
@@ -24,11 +24,7 @@ Usage: mnist_export.py [--training_iteration=x] [--model_version=y] export_dir
 
 import os
 import sys
-
-# This is a placeholder for a Google-internal import.
-
 import tensorflow as tf
-
 from tensorflow.python.saved_model import builder as saved_model_builder
 from tensorflow.python.saved_model import signature_constants
 from tensorflow.python.saved_model import signature_def_utils
@@ -37,6 +33,8 @@ from tensorflow.python.saved_model import utils
 from tensorflow.python.util import compat
 from tensorflow_serving.example import mnist_input_data
 
+
+#training flags
 tf.app.flags.DEFINE_integer('training_iteration', 1000,
                             'number of training iterations.')
 tf.app.flags.DEFINE_integer('model_version', 1, 'version number of the model.')
@@ -58,11 +56,16 @@ def main(_):
 
   # Train model
   print 'Training model...'
+  
+  #Read the data and format it
   mnist = mnist_input_data.read_data_sets(FLAGS.work_dir, one_hot=True)
   sess = tf.InteractiveSession()
   serialized_tf_example = tf.placeholder(tf.string, name='tf_example')
   feature_configs = {'x': tf.FixedLenFeature(shape=[784], dtype=tf.float32),}
   tf_example = tf.parse_example(serialized_tf_example, feature_configs)
+  
+  
+  #Build model
   x = tf.identity(tf_example['x'], name='x')  # use tf.identity() to assign name
   y_ = tf.placeholder('float', shape=[None, 10])
   w = tf.Variable(tf.zeros([784, 10]))
@@ -74,6 +77,9 @@ def main(_):
   values, indices = tf.nn.top_k(y, 10)
   table = tf.contrib.lookup.index_to_string_table_from_tensor(
       tf.constant([str(i) for i in xrange(10)]))
+  
+  
+  #train the model
   prediction_classes = table.lookup(tf.to_int64(indices))
   for _ in range(FLAGS.training_iteration):
     batch = mnist.train.next_batch(50)
@@ -85,22 +91,43 @@ def main(_):
                            y_: mnist.test.labels})
   print 'Done training!'
 
-  # Export model
-  # WARNING(break-tutorial-inline-code): The following code snippet is
-  # in-lined in tutorials, please update tutorial documents accordingly
-  # whenever code changes.
+  # Save the model
+  
+  #where to save to?
   export_path_base = sys.argv[-1]
   export_path = os.path.join(
       compat.as_bytes(export_path_base),
       compat.as_bytes(str(FLAGS.model_version)))
   print 'Exporting trained model to', export_path
+  
+  #This creates a SERVABLE from our model
+  #saves a "snapshot" of the trained model to reliable storage 
+  #so that it can be loaded later for inference.
+  #can save as many version as necessary
+  
+  #the tensoroflow serving main file tensorflow_model_server
+  #will create a SOURCE out of it, the source
+  #can house state that is shared across multiple servables 
+  #or versions
+  
+  #we can later create a LOADER from it using tf.saved_model.loader.load
+  
+  #then the MANAGER decides how to handle its lifecycle
+  
   builder = saved_model_builder.SavedModelBuilder(export_path)
 
   # Build the signature_def_map.
+  #Signature specifies what type of model is being exported, 
+  #and the input/output tensors to bind to when running inference.
+  #think of them as annotiations on the graph for serving
+  #we can use them a number of ways
+  #grabbing whatever inputs/outputs/models we want either on server
+  #or via client
   classification_inputs = utils.build_tensor_info(serialized_tf_example)
   classification_outputs_classes = utils.build_tensor_info(prediction_classes)
   classification_outputs_scores = utils.build_tensor_info(values)
 
+   
   classification_signature = signature_def_utils.build_signature_def(
       inputs={signature_constants.CLASSIFY_INPUTS: classification_inputs},
       outputs={
@@ -120,6 +147,8 @@ def main(_):
       method_name=signature_constants.PREDICT_METHOD_NAME)
 
   legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+  
+  #add the sigs to the servable
   builder.add_meta_graph_and_variables(
       sess, [tag_constants.SERVING],
       signature_def_map={
@@ -130,6 +159,7 @@ def main(_):
       },
       legacy_init_op=legacy_init_op)
 
+  #save it!
   builder.save()
 
   print 'Done exporting!'
